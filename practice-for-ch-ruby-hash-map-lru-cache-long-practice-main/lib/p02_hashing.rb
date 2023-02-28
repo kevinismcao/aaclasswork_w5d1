f53cbfb27c705d54c78a1d53ff33d6eb01d73b62
ningxiao <dsgrichard@hotmail.com>
2023-02-27 16:03:15 -0800

    p02

--- practice-for-ch-ruby-hash-map-lru-cache-long-practice-main/lib/p02_hashing.rb
+++ practice-for-ch-ruby-hash-map-lru-cache-long-practice-main/lib/p02_hashing.rb
@@ -4,11 +4,24 @@ end
 
 class Array
   def hash
+    res = 0
+    self.each_with_index do |ele, i|
+      res += ele*i
+    end
+    res
+
+
   end
 end
 
 class String
   def hash
+    res = 0
+    alph = ("a".."z").to_a
+    self.each_char.with_index do |ele, i|
+      res += (alph.index(ele.downcase) * i)
+    end
+    res
   end
 end
 
