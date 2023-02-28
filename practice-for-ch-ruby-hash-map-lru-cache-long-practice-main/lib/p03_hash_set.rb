eb571ce6a3d3af78abfcdb67c6f178bdb58fa0c8
ByronHan333 <zhan12@dons.usfca.edu>
2023-02-27 16:26:49 -0800

    Finishing Phase 3 (HashSet)

--- practice-for-ch-ruby-hash-map-lru-cache-long-practice-main/lib/p03_hash_set.rb
+++ practice-for-ch-ruby-hash-map-lru-cache-long-practice-main/lib/p03_hash_set.rb
@@ -1,5 +1,6 @@
 class HashSet
   attr_reader :count
+  attr_accessor :store
 
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { Array.new }
@@ -7,12 +8,25 @@ class HashSet
   end
 
   def insert(key)
+    if !(@count < @store.length)
+      resize!
+    end
+
+    if !include?(key)
+      @store[key.hash % @store.length] << key
+      @count += 1
+    end
   end
 
   def include?(key)
+    @store[(key.hash) % @store.length].include?(key)
   end
 
   def remove(key)
+    if include?(key)
+      @store[(key.hash) % @store.length].delete(key)
+      @count -= 1
+    end
   end
 
   private
@@ -22,9 +36,21 @@ class HashSet
   end
 
   def resize!
+      @store << []
+
+    (0..@store.length-1).each do |i|
+      if !@store[i].empty?
+        ele = @store[i].first
+        @store[i].delete(ele)
+        @store[ele.hash % @store.length] << ele
+      end
+    end
   end
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num]
   end
 end
+
+# p "howdy".hash
